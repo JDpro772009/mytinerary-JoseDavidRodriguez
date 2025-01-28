165aa00b6ecb15d305c797da689bc55373cfb3a7
jhoselerodriguez <jhoselerodriguez@gmail.com>
2025-01-27 20:47:56 -0500

    guardado

--- src/Pages/Cities.jsx
+++ src/Pages/Cities.jsx
@@ -19,7 +19,7 @@ console.log(ciudades);
 
 const dispatch = useDispatch()
 useEffect(()=>{
-  fetch("http://localhost:2020/api/cities/all")
+  fetch("https://mytinerary-back-josedavidrodriguez.onrender.com/api/cities/all")
   .then((val) => val.json())
     .then((data) => {
       dispatch(addCities({cities:data}))
